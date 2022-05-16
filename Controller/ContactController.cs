73c661fb5e558ef713a9a1c66ae23bb140a4d88e
savvita <svt13@ukr.net>
2022-05-16 20:17:28 +0300

    add contact controller

---
+++ Controller/ContactController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using ContactsMVC.Model;
+
+namespace ContactsMVC.Controller
+{
+    public class ContactController
+    {
+        private List<ContactModel> contacts;
+        public List<ContactModel> GetContacts()
+        {
+            return contacts;
+        }
+
+        public void AddContact(ContactModel contact)
+        {
+            contacts.Add(contact);
+        }
+
+        public void RemoveContact(ContactModel contact)
+        {
+            contacts.Remove(contacts.Find(x => x.ID == contact.ID));
+        }
+    }
+}
